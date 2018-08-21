a9ab80eef8b30201e913823343b7ee6f55595de7
Irene R <irene@smallwondermedia.com>
2018-08-20 21:01:12 -0400

    1

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var characters = ["Jake", "Amy", "Gina", "Rosa", "Charles", "Terry", "Holt"];
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+
+for (var = i; i < characters.length; i++) {
+    console.log("I love " + characters[i]);
+}
