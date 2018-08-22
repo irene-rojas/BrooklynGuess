9390716febd6c3c274f7640d7b0c6dfd5db9d82e
Irene R <irene@smallwondermedia.com>
2018-08-21 22:57:42 -0400

    5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,50 @@
-var characters = ["Jake", "Amy", "Gina", "Rosa", "Charles", "Terry", "Holt"];
+// var names = ["jake", "amy", "gina", "rosa", "charles", "terry", "holt"];
 
-var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+var names = ['jake', 'amy', 'gina', 'rosa', 'charles', 'terry', 'holt'];
+var wins = 0;
+var guessRemain = 0;
+var lettersGuessed 
+
+
+    document.onkeyup = function() {
+    // alert("working");
+
+    // computer chooses a name
+    var computerGuess = names[Math.floor(Math.random()*names.length)];
+
+    console.log(computerGuess);
+
+
+    // user chooses a letter
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    console.log(userGuess);
+
+    // if userGuess comes from var alphabet, they have chosen correctly on click.
+
+        if (userGuess == 'a' || userGuess == 'b' || userGuess == 'c') { 
+            // alert("chosen correctly");
+            // if userGuess is letter in computerGuess name, show letter in corresponding location.
+        } else {
+            alert("Please choose a, b, or c");
+            }
+        }
+
+    // }
+    // end function
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //  || userGuess=== 'd' || userGuess=== 'e' || userGuess=== 'f' || userGuess=== 'g' || userGuess=== 'h' || userGuess=== 'i' || userGuess=== 'j' || userGuess=== 'k' || userGuess=== 'l' || userGuess=== 'm' || userGuess=== 'n' || userGuess=== 'o' || userGuess=== 'p' || userGuess=== 'q' || userGuess=== 'r' || userGuess=== 's' || userGuess=== 't' || userGuess=== 'u' || userGuess=== 'v' || userGuess=== 'w' || userGuess=== 'x' || userGuess=== 'y' || userGuess=== 'z') {   
