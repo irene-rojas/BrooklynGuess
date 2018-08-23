ffbc49f5b6a6eaeadebc12cd271f119b55ca61d6
Irene R <irene@smallwondermedia.com>
2018-08-23 00:01:32 -0400

    6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,37 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-console.log(alphabet);
+    console.log(alphabet);
 
-// name choice options
+// name choice options - console
 var names = ['jake', 'amy', 'gina', 'rosa', 'charles', 'terry', 'holt'];
-console.log(names);
+    console.log(names);
 
-var start = document.querySelector("#start");
-
-document.getElementById("start").addEventListener("click", function() {
-    var name = names[Math.floor(Math.random()*names.length)];
+// name choice - console
+var name = names[Math.floor(Math.random()*names.length)];
     console.log(name);
 
-}
-);
+var answerArray = [];
+    for (var i = 0; i < name.length; i++) {
+        answerArray[i] = "_";
+    }
 
-start.onclick = function() {
+var remainingLetters = name.length;
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// initialize game
-document.onkeyup = function() {
-
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        // console.log(userGuess);
-
-        if ((userGuess===alphabet)) {
-            
-            // computer chooses one name
-            var name = names[Math.floor(Math.random()*names.length)];
-                console.log(name);
-
-                // show dashes instead of word
-                var answersArray = [];
-                for (var i = 0; i < name.length; i++) {
-                    answerArray[i] = "_ ";
-                }
+// the game
+while (remainingLetters > 0) {
+    console.log(answerArray.join(" "));
+    var guess = console.log("Guess a letter");
+    if (guess === null) {
+        break;
+    } else {
+        for (var j = 0; j < name.lenth; j++) {
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
         }
-
-
-
-
-
-// create key event and identify letter chosen
-// if key clicked, store letter in an array to track progress
-document.onkeyup = function(event) {
-    var letter = String.fromCharCode(event.keyCode).toLowerCase();
-
+    }
 }
 
-// save chosen letter in array
-// const
+// document.write(answerArray.join(" "));
+document.write("Detective " + name + " will see you now.");
 
-// identify whether chosen letter is in selected word
-
-// if letter is in word, replace a dash with that letter
-
-// document.getElementById
-
-
-}
-// initialize game tag
